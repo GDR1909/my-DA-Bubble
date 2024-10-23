8433148191f2850ee18e9eaf743cd51c1319f640
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2024-10-23 14:16:32 +0200

    optimized the imprint and the privacy policy

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-imprint',
@@ -10,6 +11,21 @@ import { Location } from '@angular/common';
 })
 export class ImprintComponent {
   location = inject(Location);
+
+  /**
+   * @param router - Router service used for navigation within the application.
+   */
+  constructor(private router: Router) {}
+
+  /**
+   * ngOnInit lifecycle hook for Angular component.
+   * Navigates to the '/imprint' URL and scrolls the window to the top.
+   */
+  ngOnInit() {
+    this.router.navigateByUrl('/imprint').then(() => {
+      window.scrollTo(0, 0);
+    });
+  }
 
   /**
    * Navigates back to the previous page in the browser history.
